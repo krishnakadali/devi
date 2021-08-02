0595ed26049e30f1479d69223291e2f52e0f0157
krishna kadali <krishnakadalidevops@gmail.com>
2021-08-02 10:34:44 +0530

    krishna pjct

---
+++ src/main/java/pjct/Application.java
@@ -0,0 +1,8 @@
+package pjct;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/pjct/ApplicationTest.java
@@ -0,0 +1,11 @@
+package pjct;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testMainMethod() {
+
+    }
+}
